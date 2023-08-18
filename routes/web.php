db22727f82f8503978d5e1e541ba5069b57e9385
Juan Jose <chevike15@gmail.com>
2023-08-17 23:31:55 -0600

    Creando el módulo de login con laravel/breeze y traduciendo los mensajes a español

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// ruta principal
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ruta del panel de control
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// rutas de perfil
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// 
+
+
+require __DIR__.'/auth.php';
